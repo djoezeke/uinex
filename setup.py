1b77ce49a4d46b78bcc15c176d1d84cf3433a39a
Sackey Ezekiel Etrue <sackeyetrue@gmail.com>
2025-09-17 13:25:16 +0000

    Remove alpha development status and clean up setup.py formatting

--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
+def readme():
+    fname = "README.md"
+    if os.path.exists(fname):
+        with open(fname, encoding="utf-8") as f:
+            return f.read()
+    return ""
+
+
 import os
 from setuptools import setup, find_packages
 from uinex.version import vernum
@@ -10,67 +18,66 @@ def readme():
             return f.read()
     return ""
 
+
 setup(
-    name="pygameui",
+    name="uinex",
     version=f"{vernum}",
     author="Sackey Ezekiel Etrue",
     author_email="sackeyetrue@gmail.com",
     maintainer="Sackey Ezekiel Etrue",
     maintainer_email="sackeyetrue@gmail.com",
-    description="Python Game Development User Interface (Pygame-GUI)",
+    description="Create modern looking GUIs with pygame.",
     long_description=readme(),
     long_description_content_type="text/markdown",
-    url="https://github.com/djoezeke/pygameui",
+    url="https://github.com/djoezeke/uinex",
     project_urls={
-        "Homepage": "https://github.com/djoezeke/pygameui",
-        "Documentation": "https://github.com/djoezeke/pygameui#readme",
-        "Issues": "https://github.com/djoezeke/pygameui/issues",
-        "Release Notes": "https://github.com/djoezeke/pygameui/releases",
-        "Source": "https://github.com/djoezeke/pygameui",
+        "Homepage": "https://github.com/djoezeke/uinex",
+        "Documentation": "https://github.com/djoezeke/uinex#readme",
+        "Issues": "https://github.com/djoezeke/uinex/issues",
+        "Release Notes": "https://github.com/djoezeke/uinex/releases",
+        "Source": "https://github.com/djoezeke/uinex",
     },
     license="MIT",
-    packages=find_packages(
-        where=".",
-        exclude=['test',],
-        include=["pygameui", "pygameui.core", "pygameui.widget", "pygameui.utils"]
-        ),
-    py_modules=["pygameui"],
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
     install_requires=[
-        "pygame>=2.0.0",
+        "pillow>=11.3.0",
+        "pygame",
     ],
     extras_require={
-        "dev": ["pytest", "black","isort"],
-        "docs": ["sphinx"],
+        "dev": ["pytest", "build", "twine", "wheel"],
+        "docs": [],
     },
-    python_requires=">=3.9",
+    python_requires=">=3.13",
     keywords=[
-        "pygame", "pygameui", "game ui", "2d", "games", "development", "gui", "library"
+        "pygame",
+        "uinex",
+        "game ui",
+        "2d",
+        "games",
+        "development",
+        "gui",
+        "library",
     ],
     platforms=["any"],
     classifiers=[
-        "Development Status :: 1 - Beta",
+        "Framework :: Uinex",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
-        "Programming Language :: Python :: 3.13",
         "Operating System :: Microsoft :: Windows",
-        "Operating System :: POSIX",
-        "Operating System :: Unix",
+        "Operating System :: Linux",
         "Operating System :: MacOS",
+        "Operating System :: OS Independent",
         "Topic :: Games/Entertainment",
-        "Topic :: Software Development :: Libraries :: pygame",
+        "Topic :: Multimedia :: Graphics",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
         "Typing :: Typed",
     ],
-    setup_requires=['setuptools', "wheel"],
-    options={'bdist_wheel': {'universal': False}},
-    entry_points={
-        'pyinstaller40': ['hook-dirs = pygameui.hook:get_hook_dirs'],
-    },
-    zip_safe=False,
     include_package_data=True,
+    zip_safe=False,
 )
